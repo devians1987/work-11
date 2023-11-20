5bc83f54b8eb407d82b4f82549acc1ef43168948
dev1ans <dev1ans@mail.ru>
2023-11-20 14:58:54 +0500

    two commit

---
+++ src/main/java/Movie.java
@@ -0,0 +1,14 @@
+public class Movia {
+
+    private int movieId;
+    private String movieName;
+    private String movieType;
+
+    public Movie(int movieId, String movieName, String movieType) {
+        this.movieId = movieId;
+        this.movieName = movieName;
+        this.movieType = movieType;
+    }
+}
+
+
